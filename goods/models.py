dbb295f844dc655a7d2200e7ed5f211642a196d2
Volodymyr Popov <texnolife@gmail.com>
2024-02-12 21:56:25 +0000

    Connect Cloudinary change product model

--- goods/models.py
+++ goods/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -16,7 +17,7 @@ class Products(models.Model):
     name = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="goods_images", blank=True, null=True)
+    image = CloudinaryField('image', default='placeholder')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
