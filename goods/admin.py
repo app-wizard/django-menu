64de43ca0953c080228d843fab13d675c1b33037
Volodymyr Popov <texnolife@gmail.com>
2024-02-12 21:31:41 +0000

    add category and product models

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from goods.models import Categories, Products
 
 # Register your models here.
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "price", "discount", "quantity", "category")
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
